ef1626a8f75ce6314564f7e7e990da1f8d2cd4df
Todd Powell <toddwpowell@gmail.com>
2023-09-11 14:44:09 -0600

    Update CSS for results

--- src/components/ResultsSummary.tsx
+++ src/components/ResultsSummary.tsx
@@ -4,25 +4,27 @@ const ResultsSummary = () => {
   return (
     <>
       <div className="results-summary-wrapper">
-        <div className="result-wrapper">
+        <div className="result-wrapper bg-gradient">
           <p className="result-title">Your Result</p>
-          <div className="result-score">76 of 100</div>
+          <div className="circle-wrapper">
+            <div className="result-score circle-gradient circle">76 of 100</div>
+          </div>
           <div className="result-message">
             <h1>Great</h1>
             <p>
-              You scored hither than 65% of the people who have taken these
+              You scored higher than 65% of the people who have taken these
               tests.
             </p>
           </div>
         </div>
         <div className="summary-wrapper">
           <p className="summary-title">Summary</p>
-          <ul className="summary-list">
-            <li className="summary-item">Reaction 80 / 100</li>
-            <li className="summary-item">Memory 92 / 100</li>
-            <li className="summary-item">Verbal 61 / 100</li>
-            <li className="summary-item">Visual 72 / 100</li>
-          </ul>
+          <div className="summary-list">
+            <p className="summary-item">Reaction 80 / 100</p>
+            <p className="summary-item">Memory 92 / 100</p>
+            <p className="summary-item">Verbal 61 / 100</p>
+            <p className="summary-item">Visual 72 / 100</p>
+          </div>
           <button className="continue">Continue</button>
         </div>
       </div>
